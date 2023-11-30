ae54c9d628563cbc696ffbab3340389da47104b7
dev-Jaewon <app235naver@gmail.com>
2023-12-01 00:41:17 +0900

    feat role 권한 db생성

---
+++ src/main/java/com/devjaewon/securityproject/entity/RoleEntity.java
@@ -0,0 +1,36 @@
+package com.devjaewon.securityproject.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity(name = "role")
+@Table(name = "role")
+public class RoleEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
