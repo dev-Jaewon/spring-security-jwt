aa9010a006761ebe4cea8bf9d9a82c8a1f52381a
dev-Jaewon <app235naver@gmail.com>
2023-11-30 20:24:19 +0900

    modified jwt 인증 구현

--- src/main/java/com/devjaewon/securityproject/security/JwtAuthenticationFilter.java
+++ src/main/java/com/devjaewon/securityproject/security/JwtAuthenticationFilter.java
@@ -2,20 +2,14 @@ package com.devjaewon.securityproject.security;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.apache.coyote.BadRequestException;
-import org.hibernate.mapping.Collection;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -31,43 +25,39 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     public JwtAuthenticationFilter() {
     }
 
-    // @Autowired
-    // private JwtProvider jwtProvider;
-
-    @Autowired
-    private UserDetailsService userDetailsService;
-
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-        System.out.println("do filter ---- ");
-
         String authorization = request.getHeader("Authorization");
 
         if (authorization != null && authorization.startsWith("Bearer ")) {
-            String token = authorization.substring(7);
+            try {
 
-            JwtProvider jwtProvider = new JwtProvider();
+                String token = authorization.substring(7);
 
-            String userName = jwtProvider.getUserName(token);
-            Claims claims = jwtProvider.getClaims("roles", token).getPayload();
+                JwtProvider jwtProvider = new JwtProvider();
 
-            // List<GrantedAuthority> rolesMap = claims.get("roles", List.class);
+                Claims payload = jwtProvider.getClaims(token).getPayload();
+                String userName = payload.getSubject();
 
-            // List<SimpleGrantedAuthority> roles = Arrays.stream(claims.get("roles").toString().split(","))
-            //         .map(role -> role.startsWith("ROLE_") ? role : "ROLE_" + role)
-            //         .map(SimpleGrantedAuthority::new)
-            //         .collect(Collectors.toList());
+                List<?> rolesFromPlayload = payload.get("roles", ArrayList.class);
 
-            List<SimpleGrantedAuthority> roles = Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+                List<SimpleGrantedAuthority> roles = rolesFromPlayload
+                        .stream().map(v -> "ROLE_" + v)
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
 
-            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                    userName, null, roles);
+                Authentication authentication = new UsernamePasswordAuthenticationToken(
+                        userName, null, roles);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
 
-            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+            } catch (Exception e) {
+                response.sendError(HttpStatus.UNAUTHORIZED.value(), "토큰이 만료 되었습니다.");
+                return;
+            }
         }
+
         filterChain.doFilter(request, response);
     }
 
